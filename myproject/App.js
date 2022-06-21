dfecfdb2917a878bc7dfafa6d3dcb666bf6bff6b
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-06-21 14:52:37 +0530

    commit

--- myproject/App.js
+++ myproject/App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View , TextInput} from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('pavi');
+  const [age,setAge] = useState('30');
+  
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+      <Text>Enter name:</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder='e.g. John Doe'
+      onChangeText={(val)=> setName(val)}
+      />
+
+      <Text>Enter age:</Text>
+      <TextInput 
+      keyboardType='numeric'
+      style={styles.input}
+      placeholder='e.g. 99'
+      onChangeText={(val)=> setAge(val)}
+      />
+      <Text>name: {name} , age:{age}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200,
+  },
+  
 });
