3d118687d850904afdc984ef2798b6942d74fb4d
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-06-21 15:42:53 +0530

    commit

--- myproject/App.js
+++ myproject/App.js
@@ -1,31 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {useState} from 'react';
-import { StyleSheet, Text, View , TextInput} from 'react-native';
+import { StyleSheet, Text, View , ScrollView} from 'react-native';
 
 export default function App() {
-  const [name,setName] = useState('pavi');
-  const [age,setAge] = useState('30');
+  const [people , setPeople] = useState([
+    {name:'shaun' , key:'1'},
+    {name:'shaun1' , key:'2'},
+    {name:'shaun2' , key:'3'},
+    {name:'shaun3' , key:'4'},
+    {name:'shaun4' , key:'5'},
+    {name:'shaun5' , key:'6'},
+    {name:'shaun6' , key:'7'},
+  ]);
   
 
   
 
   return (
     <View style={styles.container}> 
-      <Text>Enter name:</Text>
-      <TextInput 
-      style={styles.input}
-      placeholder='e.g. John Doe'
-      onChangeText={(val)=> setName(val)}
-      />
-
-      <Text>Enter age:</Text>
-      <TextInput 
-      keyboardType='numeric'
-      style={styles.input}
-      placeholder='e.g. 99'
-      onChangeText={(val)=> setAge(val)}
-      />
-      <Text>name: {name} , age:{age}</Text>
+      <ScrollView>
+      {people.map((item)=>(
+        
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        
+      ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -35,15 +36,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input:{
-    borderWidth:1,
-    borderColor:'#777',
-    padding:8,
-    margin:10,
-    width:200,
+    padding:40,
+    paddingHorizontal:20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   
 });
