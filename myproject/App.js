4db371913d8a98dd9796eca061db1a6f5cfc27b2
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-06-21 15:49:45 +0530

    commit

--- myproject/App.js
+++ myproject/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {useState} from 'react';
-import { StyleSheet, Text, View , ScrollView} from 'react-native';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
 
 export default function App() {
   const [people , setPeople] = useState([
@@ -18,7 +18,15 @@ export default function App() {
 
   return (
     <View style={styles.container}> 
-      <ScrollView>
+
+      <FlatList
+        data={people}
+        renderItem={({ item })=> (
+          <Text style={styles.item}>{item.name}</Text>
+        )} 
+      />
+
+      {/* <ScrollView>
       {people.map((item)=>(
         
           <View key={item.key}>
@@ -26,7 +34,7 @@ export default function App() {
           </View>
         
       ))}
-      </ScrollView>
+      </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
